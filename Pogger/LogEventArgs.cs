20433be64a2bb51b0126637fbba4befe98bb6f00
Poyo <poyoarya@gmail.com>
2022-05-08 10:22:22 -0700

    Use record instead of struct

--- Pogger/LogEventArgs.cs
+++ Pogger/LogEventArgs.cs
@@ -1,13 +1,3 @@
 ﻿namespace Pogger;
 
-public struct LogEventArgs
-{
-    public MessageType Severity;
-    public string Message;
-
-    public LogEventArgs(MessageType severity, string message)
-    {
-        this.Severity = severity;
-        this.Message = message;
-    }
-}
+public record struct LogEventArgs(MessageType Severity, string Message);
